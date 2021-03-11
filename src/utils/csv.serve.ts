07365583bd57a147c3bc955f92793078956e873a
Dzmitry_Karneyenka <Dzmitry_Karneyenka@epam.com>
2021-03-11 12:12:09 +0300

    refactor[csv.serve.ts]: update with applying the csv source and targer dirs via env variables

--- src/utils/csv.serve.ts
+++ src/utils/csv.serve.ts
@@ -3,8 +3,8 @@ import { createReadStream, createWriteStream, constants } from 'fs';
 import { join, resolve, basename } from 'path';
 import csvtojson from 'csvtojson/v2';
 
-const CSV_WORK_DIR = join(resolve('.'), 'temp', 'csv');
-const CSV_DEST_DIR = join(resolve('.'), 'temp', 'csv-parsed');
+const CSV_WORK_DIR = process.env.CSV_WORK_DIR || join(resolve('.'), 'temp', 'csv');
+const CSV_DEST_DIR = process.env.CSV_DEST_DIR || join(resolve('.'), 'temp', 'csv-parsed');
 console.debug(`Working Directory:\t[${CSV_WORK_DIR}]`);
 console.debug(`Target Directory:\t[${CSV_DEST_DIR}]`);
 
@@ -43,9 +43,9 @@ async function getListCSVFiles(path: string): Promise<any> {
 async function validatePath(path: string): Promise<void> {
     try {
         await access(path, constants.F_OK);
-        console.info(`[${path}] is accessable.`);
+        console.info(`[${path}] is accessible.`);
     } catch {
-        console.info(`[${path}] is not accessable.`);
+        console.info(`[${path}] is not accessible.`);
         await mkdir(path);
         console.debug(`[${path}] has been just created.`);
     }
