620d7777ec3960332a03505ce601f9ac248960f0
Dzmitry_Karneyenka <Dzmitry_Karneyenka@epam.com>
2021-04-05 14:17:39 +0300

    fix: typo

--- src/utils/csv.serve.stream.ts
+++ src/utils/csv.serve.stream.ts
@@ -1,10 +1,10 @@
-import { access, mkdir, readdir, rm } from 'fs/promises';
-import { createReadStream, createWriteStream, constants } from 'fs';
-import { join, resolve, basename } from 'path';
-import csvtojson from 'csvtojson/v2';
+import { access, mkdir, readdir, rm } from "fs/promises";
+import { createReadStream, createWriteStream, constants } from "fs";
+import { join, resolve, basename } from "path";
+import csvtojson from "csvtojson/v2";
 
-const CSV_WORK_DIR = process.env.CSV_WORK_DIR || join(resolve('.'), 'temp', 'csv');
-const CSV_DEST_DIR = process.env.CSV_DEST_DIR || join(resolve('.'), 'temp', 'csv-parsed');
+const CSV_WORK_DIR = process.env.CSV_WORK_DIR || join(resolve("."), "temp", "csv");
+const CSV_DEST_DIR = process.env.CSV_DEST_DIR || join(resolve("."), "temp", "csv-parsed");
 console.debug(`Working Directory:\t[${CSV_WORK_DIR}]`);
 console.debug(`Target Directory:\t[${CSV_DEST_DIR}]`);
 
@@ -18,21 +18,21 @@ async function main() {
             const filepathD = join(CSV_DEST_DIR, basename(filename) + '.txt');
             const readStream = createReadStream(filepathR);
             const writeStream = createWriteStream(filepathD);
-            const csv = csvtojson({ output: "json", noheader: false })
+            const csv = csvtojson({ output: "json", noheader: false });
             readStream.pipe(csv).pipe(writeStream);
-            csv.on('data', (data) => console.log(`Data Line: [${data}]`))
+            csv.on("data", (data) => console.log(`Data Line: [${data}]`));
             await rm(filepathR);
         } catch (error) {
-            console.error('Oups...Something went wrong!');
+            console.error("Oops...Something went wrong!");
             console.error(error.message);
         }
     });
-};
+}
 
 async function getListCSVFiles(path: string): Promise<any> {
     try {
         let names = await readdir(path);
-        names = names.filter((name: string) => name.endsWith('csv'));
+        names = names.filter((name: string) => name.endsWith("csv"));
         return names;
     } catch (error) {
         console.error(`ERROR: ${error.message}`);
@@ -49,6 +49,6 @@ async function validatePath(path: string): Promise<void> {
         await mkdir(path);
         console.debug(`[${path}] has been just created.`);
     }
-};
+}
 
 main();
