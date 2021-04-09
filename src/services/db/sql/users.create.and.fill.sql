a2d1c0c57b2f01ed730802a425ab0bd2fd1b226e
Dzmitry_Karneyenka <Dzmitry_Karneyenka@epam.com>
2021-04-09 12:20:07 +0300

    feat[users.create.and.fill.sql]: sql script file is creaded

---
+++ src/services/db/sql/users.create.and.fill.sql
@@ -0,0 +1,22 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
+    login TEXT NOT NULL,
+    password TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    isDeleted BOOLEAN DEFAULT false
+   );
+
+INSERT INTO users (login, password, age) VALUES 
+('Burger', '1q2w3e', 59),
+('Murger', '12345', 49),
+('Vurger', 'qwerty', 39),
+('Zureh', '1q2w3e', 59),
+('Pestr123', '12345', 49),
+('Elementsss094', 'qwerty', 39),
+('Vividus34', '1q2w3e', 59),
+('3892Merin', '12345', 49),
+('Zabeyski23', 'qwerty', 39); 
+
+-- DROP TABLE users;
