841701bc120a8c6aa9825673f635dd4dbc145385
Dzmitry_Karneyenka <Dzmitry_Karneyenka@epam.com>
2021-04-26 22:37:12 +0300

    feat: implement initialization/synchronization databases when service starts

--- src/loaders/database.ts
+++ src/loaders/database.ts
@@ -1,9 +1,16 @@
 import { Sequelize } from "sequelize";
+import { User } from "../data-access/orm/users";
+import { Group } from "../data-access/orm/groups";
+import { UserGroup } from "../data-access/orm/user_group";
 
 export const databaseLoader = async ({ sequelize }: { sequelize: Sequelize }): Promise<void> => {
     try {
         await sequelize.authenticate();
         console.log("Connection has been established successfully.");
+        await User.sync();
+        await Group.sync();
+        await UserGroup.sync();
+        console.log("Databases have been synchronized successfully.");
     } catch (error) {
         console.error("Unable to connect to the database:", error);
         throw new Error("Database initialization Error");
